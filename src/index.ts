3db9e42a0c6f61400732104df0fa3c8067243214
Miles Zimmerman <miles@houseninja.co>
2022-08-29 02:27:00 -0700

    release: update to capacitor 4

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { MixpanelPlugin } from './definitions';
 
-const Mixpanel = registerPlugin<MixpanelPlugin>('Mixpanel', {
-  web: () => import('./web').then(m => new m.MixpanelWeb()),
-});
+const Mixpanel = registerPlugin<MixpanelPlugin>(
+  'Mixpanel',
+  {
+    web: () => import('./web').then(m => new m.MixpanelWeb()),
+  },
+);
 
 export * from './definitions';
 export { Mixpanel };
