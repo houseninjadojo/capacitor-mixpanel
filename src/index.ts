9b477db876b60ab4f3144a5769cb648e8de1bc28
Miles Zimmerman <miles@houseninja.co>
2023-01-18 21:23:03 -0800

    chore(ci/cd): lint action

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { MixpanelPlugin } from './definitions';
 
-const Mixpanel = registerPlugin<MixpanelPlugin>(
-  'Mixpanel',
-  {
-    web: () => import('./web').then(m => new m.MixpanelWeb()),
-  },
-);
+const Mixpanel = registerPlugin<MixpanelPlugin>('Mixpanel', {
+  web: () => import('./web').then((m) => new m.MixpanelWeb()),
+});
 
 export * from './definitions';
 export { Mixpanel };
